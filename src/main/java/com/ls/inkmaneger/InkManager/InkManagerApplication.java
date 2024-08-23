3f170d82ecf7778a9cb9b8c4c67cfadd6d14395c
Lucas Soares <soareslukas9090@hotmail.com>
2024-08-23 12:12:06 -0300

    feat: esqueleto do sistema criado

---
+++ src/main/java/com/ls/inkmaneger/InkManager/InkManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ls.inkmaneger.InkManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ls/inkmaneger/InkManager/InkManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.inkmaneger.InkManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
